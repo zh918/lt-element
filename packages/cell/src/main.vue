6b0ad540cdb4a97273c06b20ec0c15062b718289
lsmin <915744317@qq.com>
2020-01-14 17:28:42 +0800

    添加cell和cell-container

---
+++ packages/cell/src/main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cell-container-full" v-if="cols && cols=='full'">
+    <div class="cell-value-full" >
+      <div :class="'cell-label-full-'+labelWidth">{{label}}</div>
+      <div class="cell-value">
+        <slot>{{value}}</slot>
+      </div>
+    </div>
+  </div>
+  <div class="cell-container" v-else>
+    <div class="no-full">
+      <div :class="'cell-label-'+labelWidth">{{label}}</div>
+      <div class="cell-value">
+        <slot>{{value}}</slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ElCell',
+  componentName: 'ElCell',
+  props: {
+    label: String,
+    value: String,
+    labelWidth: {
+      type: String,
+      default: '3'
+    },
+    cols: String
+  },
+  data() {
+    return {};
+  },
+  created() {
+    this.$props.labelWidth ? this.$props.labelWidth : '1';
+  },
+  methods: {}
+};
+</script>
